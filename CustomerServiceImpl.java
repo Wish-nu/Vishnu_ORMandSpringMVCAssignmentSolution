178896c62cc56cfe6942dc52b76740127898e120
Vishnu Prasad Preetham Kumar <81408708+Wish-nu@users.noreply.github.com>
2022-07-17 08:49:52 +0530

    Create CustomerServiceImpl.java

---
+++ CustomerServiceImpl.java
@@ -0,0 +1,39 @@
+CustomerServiceImpl.java
+package com.example.demo.service.impl;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.example.demo.entity.Customer;
+import com.example.demo.repository.CustomerRepository; 
+import com.example.demo.service.CustomerService; 
+@Service
+public class CustomerServiceImpl implements CustomerService {
+  private CustomerRepository customerRepository;
+  public CustomerServiceImpl(CustomerRepository customerRepository) {
+  super();
+  this.customerRepository = customerRepository;
+}
+  @Override
+public List<Customer> getAllCustomers() {
+
+// TODO Auto-generated method stub 
+  return customerRepository.findAll();
+}
+  
+  @Override
+  public Customer saveCustomer(Customer customer) {
+    return customerRepository.save(customer);
+}
+  @Override
+public Customer getCustomerById(Long id) {
+  return customerRepository.findById(id).get();
+}
+  @Override
+  public Customer updateCustomer(Customer customer) {
+    return customerRepository.save(customer);
+}
+  @Override
+  public void deleteCustomerById(Long id) {
+  // TODO Auto-generated method stub 
+    customerRepository.deleteById(id);
+  }
+}
